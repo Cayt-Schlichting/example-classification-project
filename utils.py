d244b633511a3a4685acf79f1d843b217ccc0ee2
Cayt Schlichting <caytschlichting@gmail.com>
2022-05-24 10:41:07 -0700

    save point

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sklearn.metrics import classification_report, accuracy_score, recall_score, precision_score, confusion_matrix, f1_score
 
 
@@ -16,7 +17,7 @@ def stats_result(p,null_h,**kwargs):
     r=kwargs.get('r',None)
     chi2=kwargs.get('chi2',None)
     alpha=kwargs.get('alpha',.05) #default value of alpha is .05
-    print(f'\n\033[1mThe null hypothesis was:\033[0m {null_h}')
+    print(f'\n\033[1mH\u2080:\033[0m {null_h}')
     
     if p < alpha: print(f"\033[1mWe reject the null hypothesis\033[0m, p = {p} | α = {alpha}")
     else: print(f"We failed to reject the null hypothesis, p = {p} | α = {alpha}")
@@ -29,14 +30,14 @@ def stats_result(p,null_h,**kwargs):
 
 
 
-def print_model_stats(act,mod,pos,**kwargs):
+def get_model_stats(act,mod,pos,**kwargs):
     """
     Gets model statistics.  Only handles binary target variables at the moment.
     Parameters:
       (R) -        act: pandas series of actual target values
       (R) -        mod: pandas series of modeled target values (must be same length as act)
       (R) -        pos: positive outcome for target variable 
-      (O) - model_dict: If model dictionary is provided, it updates dictionary with model statistics. 
+      (O) -     ret_df: If True, it returns a single row dataframe with model statistics. Index is model name.
       (O) -  to_screen: If False, model doesn't print to screen.  Default True
                         
     NOTE:  
@@ -46,7 +47,6 @@ def print_model_stats(act,mod,pos,**kwargs):
     """
     #Get return df parameter.  Default is false, which returns none and prints the statistics
     ret_df = kwargs.get('ret_df',False)
-    model_dict = kwargs.get('model_dict',None)
     to_screen = kwargs.get('to_screen',True)
     
     #Create label list - binary confusion matrix needs positive value last
@@ -75,39 +75,7 @@ def print_model_stats(act,mod,pos,**kwargs):
     support_pos = tp + fn
     support_neg = fp + tn
     
-    ###UPDATE THIS FIRST IF STATEMENT. WE WANT SOME KIND OF SERIES OR DATAFRAME TO PLAY WITH
-    #If passed a dictionary, update it:
-    # if type(model_dict) == dict:
-    #     model_dict[mod.name].update({
-    #         "Accuracy": {acc},
-    #         "precision": {precision},
-    #         "recall": {recall},
-    #         "F1": {f1},
-    #         "FNR": {fnr},
-    #         "FPR": {fpr},
-    #         "support_pos":{support_pos},
-    #         "support_neg":{support_neg},
-    #         "TP": {tp},
-    #         "FP": {fp},
-    #         "FN": {fn},
-    #         "TN": {tn},
-    #     })
-    #If passed a dictionary, update it:
-    if type(model_dict) == dict:
-        model_dict[mod.name].update({
-            "Accuracy": {acc},
-            "precision": {precision},
-            "recall": {recall},
-            "F1": {f1},
-            "FNR": {fnr},
-            "FPR": {fpr},
-            "support_pos":{support_pos},
-            "support_neg":{support_neg},
-            "TP": {tp},
-            "FP": {fp},
-            "FN": {fn},
-            "TN": {tn},
-        })
+    
     #print to screen unless user told you not to
     if to_screen:
         print(f'\033[1mModel: {mod.name}  Positive: {pos}\033[0m')
@@ -123,5 +91,24 @@ def print_model_stats(act,mod,pos,**kwargs):
         print(f'False pos rate: {fpr:.3f}')   
         print(f' Support (pos): {support_pos}')
         print(f' Support (neg): {support_neg}\n')
-    
-    return None
+
+    ##Store results in Pandas Dataframe
+    # Don't pass in df, but create a new one and concat it outside the function
+    if ret_df:
+        stats = {
+            "Accuracy": acc,
+            "precision": precision,
+            "recall": recall,
+            "F1": f1,
+            "FNR": fnr,
+            "FPR": fpr,
+            "support_pos":support_pos,
+            "support_neg":support_neg,
+            "TP": tp,
+            "FP": fp,
+            "FN": fn,
+            "TN": tn,
+        }
+        df = pd.DataFrame(data=stats,index=[mod.name])
+        return df
+    else: return None
